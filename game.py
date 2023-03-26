6c174d6b5151ef08875aadd5069de87e2abb5163
xDarkAngelXD <shettiavaneesh@gmail.com>
2023-03-25 18:03:16 -0700

    Better Hangman
    Allowing the player to guess the word directly

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 
 
-words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "testimony", "sesquipedalian", "phenomenon", "onomatopoeia", "supercalifragilisticexpialidocious", "worcestershire"]
 
 
 word = random.choice(words)
@@ -19,20 +19,25 @@ while guesses_left > 0 and "_" in hidden_word:
     print("Guesses left:", guesses_left)
 
 
-    guess = input("Guess a letter: ").lower()
+    guess = input("Guess a letter or the word directly: ").lower()
 
 
-    if guess in word:
-
-        for i in range(len(word)):
-            if word[i] == guess:
-                hidden_word[i] = guess
-        print("Correct!")
+    if guess == word:
+        hidden_word = list(word)
     else:
-        guesses_left -= 1
-        print("Wrong!")
+        # Check if the letter is in the word
+        if guess in word:
+            # Update the hidden word with the correct letter
+            for i in range(len(word)):
+                if word[i] == guess:
+                    hidden_word[i] = guess
+            print("Correct!")
+        else:
+            # Reduce the number of guesses left
+            guesses_left -= 1
+            print("Wrong!")
 
-
+# Print the outcome of the game
 if "_" not in hidden_word:
     print("Congratulations! You guessed the word:", word)
 else:
