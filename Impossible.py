86e50b7ab3005335a91ef5bc3fd71cf4266c1ca8
xDarkCoderXD <76231320+xDarkAngelXD@users.noreply.github.com>
2023-03-27 20:50:11 -0700

    Update Impossible.py
    More words

--- Impossible.py
+++ Impossible.py
@@ -1,7 +1,7 @@
 import random
 
 
-words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "testimony", "sesquipedalian", "phenomenon", "onomatopoeia", "supercalifragilisticexpialidocious", "worcestershire"]
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "testimony", "sesquipedalian", "phenomenon", "onomatopoeia", "supercalifragilisticexpialidocious", "worcestershire" 'imbe', 'jackfruit', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'pear', 'quince', 'raspberry', 'strawberry', 'tangerine']
 
 
 word = random.choice(words)
